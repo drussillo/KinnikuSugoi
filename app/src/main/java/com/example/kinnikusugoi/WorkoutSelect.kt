402ca081593ec8a88efb793884529e07734aa302
drussillo <daverussillo7@gmail.com>
2026-08-26 15:10:59 -0500

    Working on theme

--- app/src/main/java/com/example/kinnikusugoi/WorkoutSelect.kt
+++ app/src/main/java/com/example/kinnikusugoi/WorkoutSelect.kt
@@ -1,5 +1,6 @@
 package com.example.kinnikusugoi
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.snapping.SnapPosition
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,13 +10,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.FabPosition
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.scale
-import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.graphics.Color
 
 
 @Composable
@@ -27,7 +28,7 @@ fun WorkoutSelect() {
                 modifier = Modifier
                     .fillMaxWidth()
                     .statusBarsPadding(),
-                contentAlignment = Alignment.Center
+                contentAlignment = Alignment.Center,
             ) {
                 Text(text="Select Your Workout")
             }
--- app/src/main/java/com/example/kinnikusugoi/ui/theme/Theme.kt
+++ app/src/main/java/com/example/kinnikusugoi/ui/theme/Theme.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 
@@ -23,7 +24,8 @@ private val DarkColorScheme = darkColorScheme(
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
+    background = Color(0xFFFF0000),
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
